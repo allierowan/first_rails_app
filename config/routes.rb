832523621997f2ab095e7faee6421bd537994516
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-11-14 16:06:20 -0500

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/', controller: 'welcome', action: :index, as: 'index'
+
+  get '/lorem/:type', controller: 'welcome', action: :lorem, as: 'lorem'
+
+  get '/:name', controller: 'welcome', action: :show, as: 'name'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
